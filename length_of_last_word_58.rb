2e5913c3658485e64bbf0a7348c510cdd6a22a3f
f <839322915@qq.com>
2017-09-13 23:48:04 +0800

    Add problem description

--- length_of_last_word_58.rb
+++ length_of_last_word_58.rb
@@ -1,9 +1,13 @@
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' '
+# return the length of last word in the string.
+# If the last word does not exist, return 0.
+
 def length_of_last_word(s)
+  return false if s == nil || s.strip.empty?
   convert_s_to_arr = s.split(' ')
   return convert_s_to_arr.last.length
 end
 
-
 str = "They cannot be preceded by whitespace and are not as easy to spot as regular comments"
 
-p length_of_last_word(str)
+p length_of_last_word(' ')
