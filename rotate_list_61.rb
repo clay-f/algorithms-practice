68ae116b2d6c871df5630a1330d5dd3d30c4c7a4
f <839322915@qq.com>
2017-09-14 18:02:37 +0800

    Add rotate list

--- rotate_list_61.rb
+++ rotate_list_61.rb
@@ -1,5 +1,8 @@
-require 'byebug'
+require 'pry'
 # Given a list, rotate the list to the right by k places, where k is non-negative.
+# For example:
+# Given 1->2->3->4->5->NULL and k = 2,
+# return 4->5->1->2->3->NULL.
 
 class Node
   attr_accessor :val, :next
@@ -10,16 +13,30 @@ class Node
   end
 end
 
-def node_length(node)
-  if node.nil?
-    return 0
+def rotate_right(head, k)
+  convert_arr_to_node(rotate_arr(head, k))
+end
+
+def rotate_arr(head, rotate_val)
+ tmp_arr =   convert_arr(head).flatten.compact
+ rotate_index = tmp_arr.index(rotate_val) + rotate_val
+ return tmp_arr.rotate(rotate_index)
+end
+
+def convert_arr(head)
+  if head.nil?
+    return
   else
-    (1 + (node_length(node.next)))
+    Array(head.val).push(convert_arr(head.next))
   end
 end
 
-def rotate_right(head, k)
-  
+def convert_arr_to_node(arr)
+  if arr.empty?
+    return
+  else
+    Node.new(arr.first, convert_arr_to_node(arr[1..-1]))
+  end
 end
 
 one = Node.new(1)
@@ -33,7 +50,4 @@ two.next = three
 three.next = four
 four.next = five
 
-five.next = one
-
-byebug
-puts "hello world"
+p rotate_right(one, 2)
